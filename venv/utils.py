5fc8dee04eafab2f7046183921041528c6704f26
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2021-08-30 09:37:49 +0530

    Update utils.py

--- venv/utils.py
+++ venv/utils.py
@@ -3,7 +3,16 @@ import numpy as np
 import os 
 import matplotlib.pyplot as plt
 from sklearn.utils import shuffle
-imprt matplotlib.images as mpimg
+import matplotlib.images as mpimg
+from imgaug import augmenters as iaa
+import cv2
+import random
+
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D,Flatten,Dense
+from tensorflow.keras.optimizers import Adam
+
 
 def getName(filePath):
     return filePath.split('\')[-1]
@@ -112,3 +121,25 @@ def batchGen(imagesPath, steeringList, batchSize, trainFlag):
              imgBatch.append(img)
              steeringBatch.append(steering)
          yield (np.asarray(imgBatch),np.asarray(steeringBatch))
+                          
+ ##TRAINING
+                         
+def createModel():
+  model = Sequential()
+ 
+  model.add(Convolution2D(24, (5, 5), (2, 2), input_shape=(66, 200, 3), activation='elu'))
+  model.add(Convolution2D(36, (5, 5), (2, 2), activation='elu'))
+  model.add(Convolution2D(48, (5, 5), (2, 2), activation='elu'))
+  model.add(Convolution2D(64, (3, 3), activation='elu'))
+  model.add(Convolution2D(64, (3, 3), activation='elu'))
+ 
+  model.add(Flatten())
+  model.add(Dense(100, activation = 'elu'))
+  model.add(Dense(50, activation = 'elu'))
+  model.add(Dense(10, activation = 'elu'))
+  model.add(Dense(1))
+ 
+  model.compile(Adam(lr=0.0001),loss='mse')
+  return model
+model = createModel()
+model.summary()
