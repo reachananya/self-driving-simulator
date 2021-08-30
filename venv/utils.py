bfdada6e0cf0a7431032b928b83e5fb87645065a
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2021-08-30 08:50:24 +0530

    Update utils.py

--- venv/utils.py
+++ venv/utils.py
@@ -14,3 +14,35 @@ def importDataInfo(path):
     #print(data.head())
     print('Total Images Imported',data.shape[0])
     return data
+
+    
+                         
+ def balanceData(data,display=True):
+    nBin = 31
+    samplesPerBin = 500
+    hist, bins = np.histogram(data['Steering'], nBin)
+    if display:
+        center = (bins[:-1] + bins[1:]) * 0.5
+        plt.bar(center, hist, width=0.06)
+        plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
+        plt.show()
+                          
+ removeindexList = []
+    for j in range(nBin):
+        binDataList = []
+        for i in range(len(data['Steering'])):
+            if data['Steering'][i] >= bins[j] and data['Steering'][i] <= bins[j + 1]:
+                binDataList.append(i)
+        binDataList = shuffle(binDataList)
+        binDataList = binDataList[samplesPerBin:]
+        removeindexList.extend(binDataList)
+ 
+    print('Removed Images:', len(removeindexList))
+    data.drop(data.index[removeindexList], inplace=True)
+    print('Remaining Images:', len(data))
+if display:
+    hist, _ = np.histogram(data['Steering'], (nBin))
+    plt.bar(center, hist, width=0.06)
+    plt.plot((np.min(data['Steering']), np.max(data['Steering'])), (samplesPerBin, samplesPerBin))
+    plt.show()
+data = balanceData(data,display=False)
