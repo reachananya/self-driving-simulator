26221ad0388386211dbf2fa0d9d6c38cf628213a
Ananya Singhal <48484581+reachananya@users.noreply.github.com>
2021-08-30 09:01:16 +0530

    Update TrainingSimulator.py

--- venv/TrainingSimulator.py
+++ venv/TrainingSimulator.py
@@ -5,4 +5,10 @@ data = importDataInfo(path)
 
 data = balanceData(date, display=False)
 
-loadData(path,data)
+imagesPath, steering = loadData(path, data)
+
+
+xTrain, xVal, yTrain, yVal = train_test_split(imagesPath, steerings, test_size=0.2,random_state=10)
+print('Total Training Images: ',len(xTrain))
+print('Total Validation Images: ',len(xVal))
+
